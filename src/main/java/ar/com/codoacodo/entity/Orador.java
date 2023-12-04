65d8f1d9a14321ab41b69fbb9a56b5c9927ea951
Alejandro Fernandez <alejandroroberto.fernandez@gmail.com>
2023-12-03 22:36:35 -0300

    UPD 03122023 22:36

--- src/main/java/ar/com/codoacodo/entity/Orador.java
+++ src/main/java/ar/com/codoacodo/entity/Orador.java
@@ -3,126 +3,104 @@ package ar.com.codoacodo.entity;
 import java.time.LocalDate;
 
 public class Orador {
-	
-	// Las clases deben tener atributos privados y metodos publicos
 	//atributos: encapsulamiento
-	
 	private Long id;
 	private String nombre;
 	private String apellido;
 	private String mail;
 	private String tema;
-	private LocalDate fecha_alta;
+	private LocalDate fechaAlta;
 	
-	
-	//contructores (polimormismos por sobrecarga)
-	
-	public Orador(String nombre, String apellido, String mail, String tema, LocalDate fecha_alta) {
-		init(nombre, apellido, mail, tema, fecha_alta);
+	//constructor/es
+	//usar cuando voy a enviar un objeto a la db
+	//insert into orador (campos,..) values(...)
+	public Orador(String nombre, String apellido, String mail, String tema, LocalDate fechaAlta) {
+		init(nombre, apellido, mail, tema, fechaAlta);
+		//alt+shit+m
 	}
+	//alt+shit+s
 
-
-	public Orador(Long id, String nombre, String apellido, String mail, String tema, LocalDate fecha_alta) {
+	public Orador(Long id, String nombre, String apellido, String mail, String tema, LocalDate fechaAlta) {
 		this.id = id;
-		init(nombre, apellido, mail, tema, fecha_alta);
+		init(nombre, apellido, mail, tema, fechaAlta);
 	}
 	
-	private void init(String nombre, String apellido, String mail, String tema, LocalDate fecha_alta) {
+	private void init(String nombre, String apellido, String mail, String tema, LocalDate fechaAlta) {
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.mail = mail;
 		this.tema = tema;
-		this.fecha_alta = fecha_alta;
+		this.fechaAlta = fechaAlta;
 	}
 
-
-	//cambiar metodo toString() de la clase object
-	//otra forma de polimoformismo: SOBREESCRITURA, metodo que existe en una clase base (java.lang.Object)
-	@Override
+	//otra forma de polimorfismo: SOBREESCRITURA, un metodo que existe en una clase base (java.lang.Object) 
+	// pero su hijo (Orador) la cambia
 	public String toString() {
 		return "Orador [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", mail=" + mail + ", tema="
-				+ tema + ", fecha_alta=" + fecha_alta + "]";
-	}
+				+ tema + ", fechaAlta=" + fechaAlta + "]";
+	}	
 	
-	//getters y setters
+	//cambiar un metodo llamado toString() de la clase Object para ver mas lindo en la consola los atributos
+	//del objeto
+	//alt+shit+s
 	
+	//getters/setters
 	public Long getId() {
 		return this.id;
 	}
-	
-	/* No tiene sentido porque viene desde la bd y esta predefinido. No se podria cambiar
+	/*
 	public void setId(Long id) {
 		this.id = id;
 	}
 	*/
-
 	public String getNombre() {
 		return nombre;
 	}
 
-
 	public void setNombre(String nombre) {
-		if(nombre != null) {
-		this.nombre = nombre;
+		if(nombre != null) { 
+			this.nombre = nombre;
 		}else {
-			this.nombre = "N/A";
+			this.nombre = "N/D";
 		}
 	}
-
 
 	public String getApellido() {
 		return apellido;
 	}
 
-
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-
 
 	public String getMail() {
 		return mail;
 	}
 
-
 	public void setMail(String mail) {
 		this.mail = mail;
 	}
-
 
 	public String getTema() {
 		return tema;
 	}
 
-
 	public void setTema(String tema) {
 		this.tema = tema;
 	}
 
-
-	public LocalDate getFecha_alta() {
-		return fecha_alta;
+	public LocalDate getFechaAlta() {
+		return fechaAlta;
 	}
 
-
-	public void setFecha_alta(LocalDate fecha_alta) {
-		this.fecha_alta = fecha_alta;
+	public void setFechaAlta(LocalDate fechaAlta) {
+		this.fechaAlta = fechaAlta;
 	}
-
-
+	
 	public void setId(Long id) {
-		// TODO Auto-generated method stub
-		
+		this.id = id;
 	}
 	
+	///ALT+shit+s
 	
-	
-	
-	
-	
-	
-	
-	
-	
-
 }
