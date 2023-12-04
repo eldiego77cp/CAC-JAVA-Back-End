ba2a0674d26e2462db7e6b0e12df7c6473604705
Alejandro Fernandez <alejandroroberto.fernandez@gmail.com>
2023-12-03 22:22:35 -0300

    UPD 03122023 22:22

--- src/main/java/ar/com/codoacodo/repository/AdministradorDeConexiones.java
+++ src/main/java/ar/com/codoacodo/repository/AdministradorDeConexiones.java
@@ -2,41 +2,26 @@ package ar.com.codoacodo.repository;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 public class AdministradorDeConexiones {
 	
 	public Connection getConnection() {
 		
 		String username = "root";
-		String password = "secret";
-		String port = "33060"; //cambiar por el port en la base de Xampp
+		String password = "admin";
+		String port = "3306"; //cambiar por el port en la base de Xampp
 		String host = "localhost";
-		String dbName = "DB-23044"; //cambiar por el nombre en la base de Xampp
+		String dbName = "DB-23544"; //cambiar por el nombre en la base de Xampp
 		
 		String driverName = "com.mysql.cj.jdbc.Driver";
 		
 		String dbUrl = "jdbc:mysql://"+host+":"+port+":"+"/"+dbName+ "?serverTimeZone=UTC&useSSL=false";
 		
 		try {
-			Class.forName(dbName);
-			DriverManager.getConnection(driverName, username, password);
+			Class.forName(driverName);
+			return DriverManager.getConnection(dbUrl, username, password);
 		}catch(Exception e) {
-			
+			throw new IllegalArgumentException("No se pudo conectar a la db: " + dbUrl);
 		}
-		
-		
-		
-		return null;
-		
 	}
-	
-	public PreparedStatement prepared() {
-		
-		
-		return null;
-		
-	}
-
 }
