80ad8698fd0c77dcd590c9f9262d3c04f465a0aa
Alejandro Fernandez <alejandroroberto.fernandez@gmail.com>
2023-12-13 20:53:37 -0300

    UPD 13122023 21:27

--- src/main/java/ar/com/codoacodo/controllers/NuevoOradorController.java
+++ src/main/java/ar/com/codoacodo/controllers/NuevoOradorController.java
@@ -2,6 +2,11 @@ package ar.com.codoacodo.controllers;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import ar.com.codoacodo.entity.Orador;
 import ar.com.codoacodo.repository.MySqlOradorRepository;
@@ -13,7 +18,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 //http://localhost:8080/web-app-23544/api/orador/nuevo
-@WebServlet("/api/orador/nuevo")
+@WebServlet("/api/orador")
 public class NuevoOradorController extends HttpServlet{
 
 	//crear > POST
@@ -22,33 +27,37 @@ public class NuevoOradorController extends HttpServlet{
 				HttpServletResponse response /*manda el backend al frontend*/
 			) throws ServletException, IOException {
 		
-		response.addHeader("Access-Control-Allow-Origin","*");
+		//OradorRequest oradorJson = (OradorRequest )fromJSON(OradorRequest.class, request, response);
+		//obtengo el json desde el frontend
+		String json = request.getReader()
+				.lines()
+				.collect(Collectors.joining(System.lineSeparator()));//spring
 		
-		//capturo los parametros enviados por el front
-		String orador = request.getParameter("orador");//f6
-		String nombre = request.getParameter("nombre");//f6
-		String apellido = request.getParameter("apellido");
-		String email = request.getParameter("email");
-		String tema = request.getParameter("tema");
+		//convierto de json String a Objecto java usando libreria de jackson2
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new JavaTimeModule());
+		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 		
-		//validar!!!
+		OradorRequest oradorRequest = mapper.readValue(json, OradorRequest.class);
 		
-		//creo mi orador con esos paramtros
-		Orador nuevo = new Orador(nombre, apellido, email, tema, LocalDate.now());
+		//creo mi orador con esos parametros
+		Orador nuevo = new Orador(
+				oradorRequest.getNombre(), 
+				oradorRequest.getApellido(),
+				oradorRequest.getEmail(),
+				oradorRequest.getTema(),
+				LocalDate.now()
+		);
 		
 		//ahora por medio del repository guarda en la db
 		OradorRepository repository = new MySqlOradorRepository();
 		
 		repository.save(nuevo);
 		
-		//ahora respondo al front
-		//response.getWriter().print("OK");//json
+		//ahora respondo al front: json, Convirtiendo el nuevo Orador a json
+		String jsonParaEnviarALFrontend = mapper.writeValueAsString(nuevo);
+		
+		response.getWriter().print(jsonParaEnviarALFrontend);
 	}
 
-	// Preflight
-	protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.addHeader("Access-Control-Allow-Origin","*");
-	    response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, HEAD");
-	    response.addHeader("Access-Control-Allow-Headers", "X-PINGOTHER, Origin, X-Requested-With, Content-Type, Accept");
-	}
 }
---
+++ src/main/java/ar/com/codoacodo/controllers/OradorRequest.java
@@ -0,0 +1,7 @@
+package ar.com.codoacodo.controllers;
+
+public class OradorRequest {
+
+    
+    
+}
